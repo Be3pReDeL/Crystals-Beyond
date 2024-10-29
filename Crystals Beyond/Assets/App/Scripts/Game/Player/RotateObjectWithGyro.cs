2935b01902fa2ea2cbf8eb9ccab6ea3df5d28a2b
Be3pReDeL <bogatovsergey111@gmail.com>
2024-10-29 14:23:04 +0300

    Update RotateObjectWithGyro.cs

--- Crystals Beyond/Assets/App/Scripts/Game/Player/RotateObjectWithGyro.cs
+++ Crystals Beyond/Assets/App/Scripts/Game/Player/RotateObjectWithGyro.cs
@@ -5,6 +5,7 @@ public class RotateObjectWithGyro : MonoBehaviour
     private enum RotateAxis { x, y, z }
     [SerializeField] private RotateAxis _axis;
     [SerializeField] private float _multiplier = 1f;
+    [SerializeField] private float _smoothSpeed = 5f; // Скорость сглаживания
 
     private bool _gyroEnabled;
     private Gyroscope _gyro;
@@ -12,57 +13,43 @@ public class RotateObjectWithGyro : MonoBehaviour
 
     private void Start()
     {
-        // Включаем гироскоп, если он доступен
         _gyroEnabled = EnableGyro();
-
-        // Сохраняем начальные углы вращения по всем осям
-        _initialRotation = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
+        _initialRotation = transform.localEulerAngles;
     }
 
     private void Update()
     {
         if (_gyroEnabled)
         {
-            // Преобразуем данные гироскопа в углы Эйлера
             Quaternion deviceRotation = GyroToUnity(_gyro.attitude);
             Vector3 eulerAngles = deviceRotation.eulerAngles;
+            
+            // Вычисляем значение вращения по выбранной оси с учетом _multiplier и _smoothSpeed
+            float targetRotation = eulerAngles[(int)_axis] * _multiplier;
+            float smoothedRotation = Mathf.LerpAngle(
+                transform.localEulerAngles[(int)_axis], 
+                _initialRotation[(int)_axis] + targetRotation, 
+                Time.deltaTime * _smoothSpeed
+            );
 
-            switch (_axis)
-            {
-                case RotateAxis.x:
-                    float xRotation = eulerAngles.x * _multiplier; // Умножаем на _multiplier
-                    float smoothXRotation = Mathf.LerpAngle(transform.localEulerAngles.x, _initialRotation.x + xRotation, Time.deltaTime * 5f);
-                    transform.localEulerAngles = new Vector3(smoothXRotation, transform.localEulerAngles.y, transform.localEulerAngles.z);
-                    break;
-
-                case RotateAxis.y:
-                    float yRotation = eulerAngles.y * _multiplier; // Умножаем на _multiplier
-                    float smoothYRotation = Mathf.LerpAngle(transform.localEulerAngles.y, _initialRotation.y + yRotation, Time.deltaTime * 5f);
-                    transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, smoothYRotation, transform.localEulerAngles.z);
-                    break;
-
-                case RotateAxis.z:
-                    float zRotation = eulerAngles.z * _multiplier; // Умножаем на _multiplier
-                    float smoothZRotation = Mathf.LerpAngle(transform.localEulerAngles.z, _initialRotation.z + zRotation, Time.deltaTime * 5f);
-                    transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, smoothZRotation);
-                    break;
-            }
+            // Устанавливаем плавное вращение только по выбранной оси
+            Vector3 newRotation = transform.localEulerAngles;
+            newRotation[(int)_axis] = smoothedRotation;
+            transform.localEulerAngles = newRotation;
         }
     }
 
     private bool EnableGyro()
     {
-        // Проверяем, поддерживает ли устройство гироскоп
         if (SystemInfo.supportsGyroscope)
         {
             _gyro = Input.gyro;
-            _gyro.enabled = true;  // Включаем гироскоп
+            _gyro.enabled = true;
             return true;
         }
         return false;
     }
 
-    // Преобразуем данные из системы координат устройства в систему координат Unity
     private static Quaternion GyroToUnity(Quaternion q)
     {
         return new Quaternion(q.x, q.y, -q.z, -q.w);
